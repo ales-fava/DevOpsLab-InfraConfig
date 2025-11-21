b3df7f2025f834ec40c97e484d56607702ff90eb
ales-fava <alessandro.fava@hotmail.com>
2025-11-21 10:11:42 +0100

    modify sku for public ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,9 @@ resource "azurerm_public_ip" "vm_ip" {
   name                = "${var.prefix}-pip"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Static"
+
+  allocation_method   = "Static"   # Standard requiere Static
+  sku                 = "Standard" # Soluciona el error de cuota
 }
 
 resource "azurerm_network_security_group" "nsg" {
